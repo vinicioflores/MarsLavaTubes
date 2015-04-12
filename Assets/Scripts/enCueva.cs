6406ec4204fde511ace6bba9a5f053d089d2330f
vinicioflores <vfloreshdz@gmail.com>
2015-04-12 05:51:54 -0600

    Basics
    Created the basical simulation system for Mars campaign

---
+++ Assets/Scripts/enCueva.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enCueva : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		enable ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void enable(){
+		GameObject.FindWithTag("MainCamera").GetComponent<recursosCamara>().setEnCueva();
+	}
+
+	void OnTriggerStay(Collider myCollision){
+		if(myCollision.gameObject.tag == "jugador")
+			enable ();
+	}
+}
---
+++ Assets/Scripts/moverCuriosity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moverCuriosity : MonoBehaviour {
+	int[] muestra=new int[7];
+	// Use this for initialization
+	void Start () {
+		for (int i=0; i<muestra.Length; i++) {
+			muestra[i]=0;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void recogeMaterial(int lugar){
+		muestra [lugar]++;
+	}
+}
